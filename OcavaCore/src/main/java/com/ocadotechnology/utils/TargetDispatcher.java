0633ee7a170447b210703e9e6525d6e86159d432
Sarah Boyd <sarah.boyd@ocado.com>
2021-09-17 17:12:13 +0000

    feat: RDASH-2388 Move AbstractDispatcher to Ocava
    (cherry picked from commit 0c293773454ba75c48f0d0681075a57d56cc90a5)

---
+++ OcavaCore/src/main/java/com/ocadotechnology/utils/TargetDispatcher.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright © 2017-2021 Ocado (Ocava)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ocadotechnology.utils;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Provides dynamic method invocation across registered instances of a particular class. Target instances are registered
+ * with some corresponding identifier that is subsequently used to select the instance upon which to invoke a particular
+ * method.
+ *
+ * @param <I> Type of the identifiers which uniquely specify the target instances.
+ * @param <T> Type of the target instances upon which method calls will be dynamically invoked.
+ */
+@ParametersAreNonnullByDefault
+public class TargetDispatcher<I, T> {
+    private final Map<I, T> targetsById;
+
+    /**
+     * Create a TargetDispatcher by providing your own map to start with.
+     * The constructor will accept immutable maps but many of the methods in this class will not support them.
+     * @param targetsById the initial map to use.
+     */
+    public TargetDispatcher(Map<I, T> targetsById) {
+        this.targetsById = targetsById;
+    }
+
+    /**
+     * Creates a TargetDispatcher with an initial empty HashMap.
+     */
+    public TargetDispatcher() {
+        this(new HashMap<>());
+    }
+
+    /**
+     * Adds a new target to the map if there does not already exist one with the provided ID.
+     * @param id the ID of the target
+     * @param member the value of the target
+     * @return true if the target was added successfully.
+     */
+    public final boolean registerTarget(I id, T member) {
+        T existingTarget = targetsById.putIfAbsent(id, member);
+        return existingTarget == null;
+    }
+
+    /**
+     * Updates the target in the map with the provided ID, or adds a new target if the ID is not in the map.
+     * @param id the ID of the target
+     * @param member the new value to put in the map
+     * @return true if there was no existing value to update
+     */
+    public final boolean updateTarget(I id, T member) {
+        T existingTarget = targetsById.put(id, member);
+        return existingTarget == null;
+    }
+
+    /**
+     * Removes a target from the map.
+     * @param id the ID of the target to remove
+     * @return the removed target
+     */
+    public final T deregisterTarget(I id) {
+        return targetsById.remove(id);
+    }
+
+    /**
+     * Removes all targets from the map.
+     */
+    public final void deregisterAllTargets() {
+        targetsById.clear();
+    }
+
+    /**
+     * Checks if a target exists in the map.
+     * @param id the ID to check
+     * @return true if the target exists in the map.
+     */
+    public final boolean hasTarget(I id) {
+        return targetsById.containsKey(id);
+    }
+
+    /**
+     * Gets a target from the map.
+     * @param id the ID of the target to get.
+     * @return the target registered with that ID.
+     * @throws UnknownTargetException if no target is currently registered with the specified ID.
+     */
+    public T getTarget(@CheckForNull I id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Target ID cannot be NULL");
+        }
+        T target = targetsById.get(id);
+        if (target == null) {
+            throw new UnknownTargetException(id);
+        }
+        return target;
+    }
+
+    /**
+     * Optionally returns the target from the map.
+     * @param id the ID of the target to get.
+     * @return an optional containing the target, or empty if no target in the map has that ID.
+     */
+    public Optional<T> maybeGetTarget(I id) {
+        return Optional.ofNullable(targetsById.get(id));
+    }
+
+    /**
+     * Performs a provided action on the target registered with the provided ID.
+     * @param id the ID of the target to get.
+     * @param action an action to perform on the target.
+     * @throws IllegalArgumentException if the specified ID is NULL.
+     * @throws UnknownTargetException if no target is currently registered with the specified ID.
+     * @throws NullPointerException if the specified action is NULL.
+     */
+    public final void doTargetAction(I id, Consumer<T> action) {
+        action.accept(getTarget(id));
+    }
+
+    /**
+     * Performs a provided action on the target registered with the provided ID if one exists.
+     * @param id the ID of the target to get.
+     * @param action an action to perform on the target.
+     * @throws NullPointerException if the specified action is NULL.
+     */
+    public final void doTargetActionIfRegistered(I id, Consumer<T> action) {
+        T target = targetsById.get(id);
+        if (target != null) {
+            action.accept(target);
+        }
+    }
+
+    /**
+     * Performs a function on the target registered with the provided ID.
+     * @param id the ID of the target to get.
+     * @param function the function to apply to the target.
+     * @param <R> the return type of the function.
+     * @return the result of applying the function to the target.
+     * @throws IllegalArgumentException if the specified ID is NULL.
+     * @throws UnknownTargetException if no target is currently registered with the specified ID.
+     * @throws NullPointerException if the specified function is NULL.
+     */
+    public final <R> R doTargetFunction(I id, Function<T, R> function) {
+        return function.apply(getTarget(id));
+    }
+
+    /**
+     * Makes a copy (in case action causes deregistration), and calls the action for all targets.
+     * Use deregisterAllTargets to deregister without performing an action.
+     * @param action the action to apply.
+     */
+    public final void doForAllTargets(Consumer<T> action) {
+        Collection<T> targets = ImmutableList.copyOf(targetsById.values());
+        targets.forEach(action);
+    }
+
+    /**
+     * Makes a copy (in case action causes deregistration), and calls the action for all targets matching the filter.
+     * Use deregisterAllTargets to deregister without performing an action.
+     * @param filter a predicate to filter targets to apply the action to.
+     * @param action the action to apply.
+     */
+    public final void doForAllTargetsWhere(Predicate<T> filter, Consumer<T> action) {
+        Collection<T> targets = ImmutableList.copyOf(targetsById.values());
+        targets.stream().filter(filter).forEach(action);
+    }
+
+    /**
+     * Makes a copy (in case action causes deregistration), and performs a function on all targets.
+     * @param mapper the function to apply to all targets.
+     * @param <R> the return type of the function.
+     * @return a stream containing the results of applying the function.
+     */
+    public final <R> Stream<R> mapAllTargets(Function<T, R> mapper) {
+        Collection<T> targets = ImmutableList.copyOf(targetsById.values());
+        return targets.stream().map(mapper);
+    }
+
+    /**
+     * Gets an immutable copy of all targets in the map.
+     * @return all targets in the map.
+     */
+    public final Map<I, T> getAllTargets() {
+        return ImmutableMap.copyOf(targetsById);
+    }
+
+    /**
+     * Makes a copy of all targets and streams them.
+     * @return a stream containing the targets.
+     */
+    public final Stream<T> streamAllTargets() {
+        return ImmutableList.copyOf(targetsById.values()).stream();
+    }
+
+    /**
+     * Makes a copy (in case filter causes deregistration), and filters all targets using the provided predicate.
+     * @param filter the filter to apply.
+     * @return a stream containing the filtered targets.
+     */
+    public final Stream<T> filterAllTargets(Predicate<T> filter) {
+        Collection<T> targets = ImmutableList.copyOf(targetsById.values());
+        return targets.stream().filter(filter);
+    }
+
+    /**
+     * Makes a copy (in case predicate causes deregistration), and checks if any target matches the provided predicate.
+     * @param predicate the predicate to check.
+     * @return true if any target matches the predicate.
+     */
+    public final boolean anyTargetMatches(Predicate<T> predicate) {
+        Collection<T> targets = ImmutableList.copyOf(targetsById.values());
+        return targets.stream().anyMatch(predicate);
+    }
+
+    /**
+     * Exception thrown when an attempt to get a target from the map is made but that ID does not exist in the map.
+     */
+    public static final class UnknownTargetException extends RuntimeException {
+        private UnknownTargetException(Object targetId) {
+            super("No target registered with ID [" + targetId + "]");
+        }
+    }
+}
---
+++ OcavaCore/src/test/java/com/ocadotechnology/utils/TargetDispatcherTest.java
@@ -0,0 +1,298 @@
+/*
+ * Copyright © 2017-2021 Ocado (Ocava)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ocadotechnology.utils;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableSet;
+import com.ocadotechnology.utils.TargetDispatcher.UnknownTargetException;
+
+class TargetDispatcherTest {
+    private TargetDispatcher<UUID, FunObject> dispatcher;
+
+    @BeforeEach
+    void setUp() {
+        dispatcher = new TargetDispatcher<>();
+    }
+
+    @Test
+    void givenIdExists_whenTargetIsRegistered_thenReturnsFalse() {
+        FunObject funObject = new FunObject(10);
+        FunObject funObject2 = new FunObject(5);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+        assertFalse(dispatcher.registerTarget(funObjectId, funObject2));
+    }
+
+    @Test
+    void givenIdDoesNotExist_whenTargetIsRegistered_thenReturnsTrue() {
+        FunObject funObject = new FunObject(5);
+        UUID funObjectId = UUID.randomUUID();
+        assertTrue(dispatcher.registerTarget(funObjectId, funObject));
+    }
+
+    @Test
+    void whenTargetIsRegistered_thenHasTarget() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+        assertTrue(dispatcher.hasTarget(funObjectId));
+    }
+
+    @Test
+    void whenTargetIsRegistered_thenGetTargetReturnsTarget() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+        assertEquals(funObject, dispatcher.getTarget(funObjectId));
+        assertTrue(dispatcher.maybeGetTarget(funObjectId).isPresent());
+    }
+
+    @Test
+    void whenMultipleTargetsRegistered_thenGetTargetReturnsCorrectTarget() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(5));
+        dispatcher.registerTarget(funObjectId, funObject);
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(25));
+        assertEquals(funObject, dispatcher.getTarget(funObjectId));
+    }
+
+    @Test
+    void whenMultipleTargetsRegistered_thenGetAllTargetsReturnsAllTargets() {
+        FunObject funObject = new FunObject(10);
+        FunObject funObject2 = new FunObject(5);
+        FunObject funObject3 = new FunObject(25);
+        dispatcher.registerTarget(UUID.randomUUID(), funObject);
+        dispatcher.registerTarget(UUID.randomUUID(), funObject2);
+        dispatcher.registerTarget(UUID.randomUUID(), funObject3);
+        assertEquals(3, dispatcher.getAllTargets().size());
+        assertTrue(dispatcher.getAllTargets().values().containsAll(ImmutableSet.of(funObject, funObject2, funObject3)));
+    }
+
+    @Test
+    void givenMultipleTargetsRegistered_whenTargetDeregistered_thenGetTargetThrows() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(5));
+        dispatcher.registerTarget(funObjectId, funObject);
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(25));
+
+        assertEquals(funObject, dispatcher.deregisterTarget(funObjectId));
+        assertThrows(UnknownTargetException.class, () -> dispatcher.getTarget(funObjectId));
+    }
+
+    @Test
+    void givenMultipleTargetsRegistered_whenAllTargetsDeregistered_thenGetAllTargetReturnsEmpty() {
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(5));
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(10));
+        dispatcher.registerTarget(UUID.randomUUID(), new FunObject(25));
+
+        dispatcher.deregisterAllTargets();
+        assertTrue(dispatcher.getAllTargets().isEmpty());
+    }
+
+    @Test
+    void whenDoTargetAction_thenTargetUpdated() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        dispatcher.doTargetAction(funObjectId, fo -> fo.number++);
+        assertEquals(11, dispatcher.getTarget(funObjectId).number);
+    }
+
+    @Test
+    void givenNoTargets_whenDoTargetAction_thenThrows() {
+        assertThrows(UnknownTargetException.class, () -> dispatcher.doTargetAction(UUID.randomUUID(), fo -> fo.number++));
+    }
+
+    @Test
+    void whenDoTargetActionIfRegistered_thenTargetUpdated() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        dispatcher.doTargetActionIfRegistered(funObjectId, fo -> fo.number++);
+        assertEquals(11, dispatcher.getTarget(funObjectId).number);
+    }
+
+    @Test
+    void givenNoTargets_whenDoTargetActionIfRegistered_thenDoesNotThrow() {
+        assertDoesNotThrow(() -> dispatcher.doTargetActionIfRegistered(UUID.randomUUID(), fo -> fo.number++));
+    }
+
+    @Test
+    void whenTargetUpdated_thenGetTargetReturnsLatest() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject newFunObject = new FunObject(15);
+        dispatcher.updateTarget(funObjectId, newFunObject);
+        assertEquals(newFunObject, dispatcher.getTarget(funObjectId));
+    }
+
+    @Test
+    void whenDoTargetFunction_thenTargetUpdated() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        assertEquals(10, (int) dispatcher.doTargetFunction(funObjectId, fo -> fo.number));
+    }
+
+    @Test
+    void givenNoTargets_whenDoTargetFunction_thenThrows() {
+        assertThrows(UnknownTargetException.class, () -> dispatcher.doTargetFunction(UUID.randomUUID(), fo -> fo.number));
+    }
+
+    @Test
+    void whenDoForAllTargets_thenAllTargetsUpdated() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        dispatcher.doForAllTargets(fo -> fo.number++);
+        assertEquals(11, dispatcher.getTarget(funObjectId).number);
+        assertEquals(13, dispatcher.getTarget(funObjectId2).number);
+    }
+
+    @Test
+    void whenDoForAllTargetsWhere_thenMatchingTargetsUpdated() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        FunObject funObject3 = new FunObject(13);
+        UUID funObjectId3 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId3, funObject3);
+
+        dispatcher.doForAllTargetsWhere(fo -> fo.number % 2 == 0, fo -> fo.number++);
+        assertEquals(11, dispatcher.getTarget(funObjectId).number);
+        assertEquals(13, dispatcher.getTarget(funObjectId2).number);
+        assertEquals(13, dispatcher.getTarget(funObjectId3).number);
+    }
+
+    @Test
+    void whenMapAllTargets_thenMappedTargetsReturned() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        assertEquals(ImmutableSet.of(11, 13), dispatcher.mapAllTargets(fo -> fo.number + 1).collect(ImmutableSet.toImmutableSet()));
+    }
+
+    @Test
+    void whenFilterAllTargets_thenMatchingTargetsReturned() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        FunObject funObject3 = new FunObject(13);
+        UUID funObjectId3 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId3, funObject3);
+
+        assertEquals(ImmutableSet.of(funObject, funObject2, funObject3), dispatcher.streamAllTargets().collect(ImmutableSet.toImmutableSet()));
+    }
+
+    @Test
+    void whenStreamAllTargets_thenAllTargetsReturned() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        FunObject funObject3 = new FunObject(13);
+        UUID funObjectId3 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId3, funObject3);
+
+        assertEquals(ImmutableSet.of(funObject, funObject2), dispatcher.filterAllTargets(fo -> fo.number % 2 == 0).collect(ImmutableSet.toImmutableSet()));
+    }
+
+    @Test
+    void givenMatchingTargets_whenAnyTargetMatches_thenReturnsTrue() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        assertTrue(dispatcher.anyTargetMatches(fo -> fo.number % 3 == 0));
+    }
+
+    @Test
+    void givenNoMatchingTargets_whenAnyTargetMatches_thenReturnsFalse() {
+        FunObject funObject = new FunObject(10);
+        UUID funObjectId = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId, funObject);
+
+        FunObject funObject2 = new FunObject(12);
+        UUID funObjectId2 = UUID.randomUUID();
+        dispatcher.registerTarget(funObjectId2, funObject2);
+
+        assertFalse(dispatcher.anyTargetMatches(fo -> fo.number % 2 == 1));
+    }
+
+    private static class FunObject {
+        public int number;
+
+        FunObject(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(number);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof FunObject && ((FunObject) obj).number == this.number;
+        }
+    }
+}
