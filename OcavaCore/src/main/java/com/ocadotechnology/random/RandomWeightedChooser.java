425628f3cc365fcc881e12aea4dbb571bd301a78
George Simms <george.simms@ocado.com>
2022-03-01 10:59:32 +0000

    feat: RandomWeightedChooser should use instanced repeatable random
    (cherry picked from commit 3af940a1abaef68649e3fe47de6deca0b09dbafc)

--- OcavaCore/src/main/java/com/ocadotechnology/random/RandomWeightedChooser.java
+++ OcavaCore/src/main/java/com/ocadotechnology/random/RandomWeightedChooser.java
@@ -16,6 +16,9 @@
 package com.ocadotechnology.random;
 
 import java.util.Map.Entry;
+import java.util.Optional;
+
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
@@ -25,23 +28,40 @@ import com.ocadotechnology.validation.Failer;
  * Utility to choose randomly from a supplied set of items, choosing each in proportion to the provided weight of that
  * item. Never returns null, but preconditions on being supplied invalid arguments.
  */
+@ParametersAreNonnullByDefault
 public class RandomWeightedChooser<E> {
+    private final Optional<InstancedRepeatableRandom> instancedRepeatableRandom;
     private final ImmutableMap<E, Double> itemsByWeight;
     private final double sumOfWeights;
 
     public RandomWeightedChooser(ImmutableMap<E, Double> itemsByWeight) {
+        this(itemsByWeight, Optional.empty());
+    }
+
+    public RandomWeightedChooser(
+            ImmutableMap<E, Double> itemsByWeight,
+            InstancedRepeatableRandom instancedRepeatableRandom) {
+        this(itemsByWeight, Optional.of(instancedRepeatableRandom));
+    }
+
+    public RandomWeightedChooser(
+            ImmutableMap<E, Double> itemsByWeight,
+            Optional<InstancedRepeatableRandom> instancedRepeatableRandom) {
         Preconditions.checkArgument(!itemsByWeight.isEmpty(),
                 "Must supply non-empty set of items from which to choose");
         itemsByWeight.forEach((item, weight) -> Preconditions.checkState(weight >= 0,
                 "Negative weight supplied for item %s - cannot correspond to a probability", item));
         this.itemsByWeight = itemsByWeight;
+        this.instancedRepeatableRandom = instancedRepeatableRandom;
         sumOfWeights = itemsByWeight.values().stream().mapToDouble(w -> w).sum();
         Preconditions.checkArgument(sumOfWeights > 0,
                 "Non-empty item set should have at least one positive weight");
     }
 
     public E choose() {
-        double rand = RepeatableRandom.nextDouble() * sumOfWeights;
+        double nextDouble = instancedRepeatableRandom.map(InstancedRepeatableRandom::nextDouble)
+                .orElseGet(RepeatableRandom::nextDouble);
+        double rand = nextDouble * sumOfWeights;
         double cumulativeSum = 0;
         for (Entry<E, Double> entry : itemsByWeight.entrySet()) {
             cumulativeSum += entry.getValue();
--- OcavaCore/src/test/java/com/ocadotechnology/random/RandomWeightedChooserTest.java
+++ OcavaCore/src/test/java/com/ocadotechnology/random/RandomWeightedChooserTest.java
@@ -23,30 +23,62 @@ import com.google.common.collect.ImmutableMap;
 public class RandomWeightedChooserTest {
     @Test
     public void whenWeightIsZero_thenDoesNotChooseItem() {
-        RandomWeightedChooser chooser = new RandomWeightedChooser<>(ImmutableMap.of("A", 0d, "B", 1d, "C", 1d));
+        RandomWeightedChooser<String> chooser = new RandomWeightedChooser<>(ImmutableMap.of("A", 0d, "B", 1d, "C", 1d));
         RepeatableRandom.initialiseWithFixedValue(0);
         Assertions.assertNotEquals(chooser.choose(), "A");
         RepeatableRandom.initialiseWithFixedValue(0.01);
         Assertions.assertNotEquals(chooser.choose(), "A");
     }
 
+    private static final ImmutableMap<String, Double> WEIGHTED_MAP =
+            ImmutableMap.of("A", 1d, "B", 2d, "C", 3d);
+
+    private static final Double SUM =
+            WEIGHTED_MAP.values().stream().mapToDouble(w -> w).sum();
+
+    private RandomWeightedChooser<String> createWeightedChooser(double seed) {
+        return new RandomWeightedChooser<>(WEIGHTED_MAP, InstancedRepeatableRandom.fromFixedValue(seed));
+    }
+
+    @Test
+    public void whenWeightIsZero_thenDoesNotChooseItemUsingInstancedRandom() {
+        RandomWeightedChooser<String> chooser = new RandomWeightedChooser<>(
+                ImmutableMap.of("A", 0d, "B", 1d, "C", 1d),
+                InstancedRepeatableRandom.fromFixedValue(0)
+        );
+        Assertions.assertNotEquals(chooser.choose(), "A");
+        chooser = new RandomWeightedChooser<>(
+                ImmutableMap.of("A", 0d, "B", 1d, "C", 1d),
+                InstancedRepeatableRandom.fromFixedValue(0.01)
+        );
+        Assertions.assertNotEquals(chooser.choose(), "A");
+    }
+
     @Test
     public void whenRepeatableRandomReturnsSpecificValue_thenReturnsAppropriateItem() {
-        ImmutableMap<String, Double> weightedMap = ImmutableMap.of("A", 1d, "B", 2d, "C", 3d);
-        RandomWeightedChooser chooser = new RandomWeightedChooser<>(weightedMap);
-        double sum = weightedMap.values().stream().mapToDouble(w -> w).sum();
+        RandomWeightedChooser<String> chooser = new RandomWeightedChooser<>(WEIGHTED_MAP);
 
-        RepeatableRandom.initialiseWithFixedValue(0 / sum);
+        RepeatableRandom.initialiseWithFixedValue(0 / SUM);
         Assertions.assertEquals(chooser.choose(), "A");
-        RepeatableRandom.initialiseWithFixedValue(0.9 / sum);
+        RepeatableRandom.initialiseWithFixedValue(0.9 / SUM);
         Assertions.assertEquals(chooser.choose(), "A");
-        RepeatableRandom.initialiseWithFixedValue(1.1 / sum);
+        RepeatableRandom.initialiseWithFixedValue(1.1 / SUM);
         Assertions.assertEquals(chooser.choose(), "B");
-        RepeatableRandom.initialiseWithFixedValue(2.9 / sum);
+        RepeatableRandom.initialiseWithFixedValue(2.9 / SUM);
         Assertions.assertEquals(chooser.choose(), "B");
-        RepeatableRandom.initialiseWithFixedValue(3.1 / sum);
+        RepeatableRandom.initialiseWithFixedValue(3.1 / SUM);
         Assertions.assertEquals(chooser.choose(), "C");
-        RepeatableRandom.initialiseWithFixedValue(5.9 / sum);
+        RepeatableRandom.initialiseWithFixedValue(5.9 / SUM);
         Assertions.assertEquals(chooser.choose(), "C");
+    }
+
+    @Test
+    public void whenInstancedRepeatableRandomReturnsSpecificValue_thenReturnsAppropriateItem() {
+        Assertions.assertEquals(createWeightedChooser(0 / SUM).choose(), "A");
+        Assertions.assertEquals(createWeightedChooser(0.9 / SUM).choose(), "A");
+        Assertions.assertEquals(createWeightedChooser(1.1 / SUM).choose(), "B");
+        Assertions.assertEquals(createWeightedChooser(2.9 / SUM).choose(), "B");
+        Assertions.assertEquals(createWeightedChooser(3.1 / SUM).choose(), "C");
+        Assertions.assertEquals(createWeightedChooser(5.9 / SUM).choose(), "C");
     }
 }
