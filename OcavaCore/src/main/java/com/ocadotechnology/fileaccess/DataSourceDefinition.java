d0aad1cec59b782dc7bf2007526b9672c9243949
Callum Kilby <callum.kilby@ocado.com>
2024-03-19 14:33:29 +0000

    feat: #436 add a configuration option to DataSourceDefinition to specify the full path to a remote file. This is intended to take precedence over the individual bucket and key configs for remote file access where it is preferred to specify a full file path, or the concept of a bucket is not defined
    (cherry picked from commit f9d9230abc4892f63e13d6e07f95f4c517dfc4aa)

--- OcavaCore/src/main/java/com/ocadotechnology/fileaccess/DataSourceDefinition.java
+++ OcavaCore/src/main/java/com/ocadotechnology/fileaccess/DataSourceDefinition.java
@@ -15,6 +15,8 @@
  */
 package com.ocadotechnology.fileaccess;
 
+import javax.annotation.CheckForNull;
+
 public class DataSourceDefinition <E extends Enum<E>> {
     /**
      * Config key indicating the mode which should be used to find the file.
@@ -32,12 +34,26 @@ public class DataSourceDefinition <E extends Enum<E>> {
      * Config key indicating the s3 key of the desired file.
      */
     public final E key;
+    /**
+     * Config key indicating the full path to a remote file. Intended to take precedence over the individual bucket and key config.
+     * To preserve backwards compatibility this may be null.
+     */
+    @CheckForNull
+    public final E remoteFile;
 
+    /**
+     * Constructor provided for backwards compatibility with applications which don't specify remoteFile
+     */
     public DataSourceDefinition(E mode, E localFile, E bucket, E key) {
+        this(mode, localFile, bucket, key, null);
+    }
+
+    public DataSourceDefinition(E mode, E localFile, E bucket, E key, E remoteFile) {
         this.mode = mode;
         this.localFile = localFile;
         this.bucket = bucket;
         this.key = key;
+        this.remoteFile = remoteFile;
     }
 }
 
