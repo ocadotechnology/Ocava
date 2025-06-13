35c080cbc8108e924e8fd653efbb29304ba4e38f
Sarah Boyd <sarah.boyd@ocado.com>
2025-06-13 15:29:35 +0000

    feat: RDASH-6509 type safe Types
    (cherry picked from commit b71885a57f3f8f51bf6c613aff3c979ef2ed7eee)

--- OcavaCore/src/main/java/com/ocadotechnology/utils/Types.java
+++ OcavaCore/src/main/java/com/ocadotechnology/utils/Types.java
@@ -30,6 +30,199 @@ public class Types {
     /** This is a static utility and should not be constructed. */
     private Types() {}
 
+    /**
+     * Cast all elements in the collection to the provided class and return them in a stream.
+     * Filters out any element that cannot be cast.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           a stream of the cast objects
+     */
+    public static <R, T extends R> Stream<T> safeStreamInstancesOfType(Collection<R> collection, Class<T> clazz) {
+        return safeStreamInstancesOfType(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in a stream.
+     * Filters out any element that cannot be cast.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       a stream of the cast objects
+     */
+    @SuppressWarnings("unchecked") // This is actually checked
+    public static <R, T extends R> Stream<T> safeStreamInstancesOfType(Stream<R> stream, Class<T> clazz) {
+        return stream.filter(o -> clazz.isAssignableFrom(o.getClass()))
+                .map(o -> (T) o);
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return them in an ImmutableList.
+     * Filters out any element that cannot be cast.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           an ImmutableList of the cast objects
+     */
+    public static <R, T extends R> ImmutableList<T> safeGetInstancesOfType(Collection<R> collection, Class<T> clazz) {
+        return safeGetInstancesOfType(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in an ImmutableList.
+     * Filters out any element that cannot be cast.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       an ImmutableList of the cast objects
+     */
+    public static <R, T extends R> ImmutableList<T> safeGetInstancesOfType(Stream<R> stream, Class<T> clazz) {
+        return safeStreamInstancesOfType(stream, clazz).collect(ImmutableList.toImmutableList());
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return the stream.
+     * Fails if any element cannot be cast.
+     * @param collection the stream to cast
+     * @param clazz      the class to cast to
+     * @return           a stream of the cast objects
+     */
+    public static <R, T extends R> Stream<T> safeStreamInstancesOfTypeOrFail(Collection<R> collection, Class<T> clazz) {
+        return safeStreamInstancesOfTypeOrFail(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return the stream.
+     * Fails if any element cannot be cast.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       a stream of the cast objects
+     */
+    public static <R, T extends R> Stream<T> safeStreamInstancesOfTypeOrFail(Stream<R> stream, Class<T> clazz) {
+        return stream.map(i -> Types.fromTypeOrFail(i, clazz));
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return them in an ImmutableList.
+     * Fails if any element cannot be cast.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           an ImmutableList of the cast objects
+     */
+    public static <R, T extends R> ImmutableList<T> safeGetInstancesOfTypeOrFail(Collection<R> collection, Class<T> clazz) {
+        return safeGetInstancesOfTypeOrFail(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in an ImmutableList.
+     * Fails if any element cannot be cast.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       an ImmutableList of the cast objects
+     */
+    public static <R, T extends R> ImmutableList<T> safeGetInstancesOfTypeOrFail(Stream<R> stream, Class<T> clazz) {
+        return safeStreamInstancesOfTypeOrFail(stream, clazz).collect(ImmutableList.toImmutableList());
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return them in a stream.
+     * Filters out any element that cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           a stream of the cast objects
+     */
+    public static <T> Stream<T> unsafeStreamInstancesOfType(Collection<?> collection, Class<T> clazz) {
+        return unsafeStreamInstancesOfType(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in a stream.
+     * Filters out any element that cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       a stream of the cast objects
+     */
+    @SuppressWarnings("unchecked") // This is actually checked
+    public static <T> Stream<T> unsafeStreamInstancesOfType(Stream<?> stream, Class<T> clazz) {
+        return stream.filter(o -> clazz.isAssignableFrom(o.getClass()))
+                .map(o -> (T) o);
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return them in an ImmutableList.
+     * Filters out any element that cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           an ImmutableList of the cast objects
+     */
+    public static <T> ImmutableList<T> unsafeGetInstancesOfType(Collection<?> collection, Class<T> clazz) {
+        return unsafeGetInstancesOfType(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in an ImmutableList.
+     * Filters out any element that cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       an ImmutableList of the cast objects
+     */
+    public static <T> ImmutableList<T> unsafeGetInstancesOfType(Stream<?> stream, Class<T> clazz) {
+        return unsafeStreamInstancesOfType(stream, clazz).collect(ImmutableList.toImmutableList());
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return the stream.
+     * Fails if any element cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param collection the stream to cast
+     * @param clazz      the class to cast to
+     * @return           a stream of the cast objects
+     */
+    public static <T> Stream<T> unsafeStreamInstancesOfTypeOrFail(Collection<?> collection, Class<T> clazz) {
+        return unsafeStreamInstancesOfTypeOrFail(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return the stream.
+     * Fails if any element cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       a stream of the cast objects
+     */
+    public static <T> Stream<T> unsafeStreamInstancesOfTypeOrFail(Stream<?> stream, Class<T> clazz) {
+        return stream.map(i -> Types.fromTypeOrFail(i, clazz));
+    }
+
+    /**
+     * Cast all elements in the collection to the provided class and return them in an ImmutableList.
+     * Fails if any element cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param collection the collection to cast
+     * @param clazz      the class to cast to
+     * @return           an ImmutableList of the cast objects
+     */
+    public static <T> ImmutableList<T> unsafeGetInstancesOfTypeOrFail(Collection<?> collection, Class<T> clazz) {
+        return unsafeGetInstancesOfTypeOrFail(collection.stream(), clazz);
+    }
+
+    /**
+     * Cast all elements in the stream to the provided class and return them in an ImmutableList.
+     * Fails if any element cannot be cast.
+     * There is no checking on the types in the stream and the type on the class relate in any way.
+     * @param stream the stream to cast
+     * @param clazz  the class to cast to
+     * @return       an ImmutableList of the cast objects
+     */
+    public static <T> ImmutableList<T> unsafeGetInstancesOfTypeOrFail(Stream<?> stream, Class<T> clazz) {
+        return unsafeStreamInstancesOfTypeOrFail(stream, clazz).collect(ImmutableList.toImmutableList());
+    }
+
+    /**
+     * Cast the provided object to the provided class and return an empty Optional if this is not possible.
+     * Note this does not have safe and unsafe variants, if you set the object type to be R with T extends R,
+     * then R can always default to `Object`.
+     */
     @SuppressWarnings("unchecked") // This is actually checked
     public static <T> Optional<T> fromType(Object o, Class<T> clazz) {
         if (o == null) {
@@ -38,6 +231,11 @@ public class Types {
         return clazz.isAssignableFrom(o.getClass()) ? Optional.of((T) o) : Optional.empty();
     }
 
+    /**
+     * Cast the provided object to the provided class, and fail if the cast is not possible.
+     * Note this does not have safe and unsafe variants, if you set the object type to be R with T extends R,
+     * then R can always default to `Object`.
+     */
     @SuppressWarnings("unchecked") // This is actually checked
     public static <T> T fromTypeOrFail(Object o, Class<T> clazz) {
         Preconditions.checkNotNull(o, "Cannot cast null object");
@@ -45,36 +243,44 @@ public class Types {
         return (T) o;
     }
 
+    @Deprecated
     public static <T> Stream<T> streamInstancesOfType(Collection<?> collection, Class<T> clazz) {
         return streamInstancesOfType(collection.stream(), clazz);
     }
 
+    @Deprecated
     @SuppressWarnings("unchecked") // This is actually checked
     public static <T> Stream<T> streamInstancesOfType(Stream<?> stream, Class<T> clazz) {
         return stream.filter(o -> clazz.isAssignableFrom(o.getClass()))
                 .map(o -> (T) o);
     }
 
+    @Deprecated
     public static <T> ImmutableList<T> getInstancesOfType(Collection<?> collection, Class<T> clazz) {
         return getInstancesOfType(collection.stream(), clazz);
     }
 
+    @Deprecated
     public static <T> ImmutableList<T> getInstancesOfType(Stream<?> stream, Class<T> clazz) {
         return streamInstancesOfType(stream, clazz).collect(ImmutableList.toImmutableList());
     }
 
+    @Deprecated
     public static <T> Stream<T> streamInstancesOfTypeOrFail(Collection<?> collection, Class<T> clazz) {
         return streamInstancesOfTypeOrFail(collection.stream(), clazz);
     }
 
+    @Deprecated
     public static <T> Stream<T> streamInstancesOfTypeOrFail(Stream<?> stream, Class<T> clazz) {
         return stream.map(i -> Types.fromTypeOrFail(i, clazz));
     }
 
+    @Deprecated
     public static <T> ImmutableList<T> getInstancesOfTypeOrFail(Collection<?> collection, Class<T> clazz) {
         return getInstancesOfTypeOrFail(collection.stream(), clazz);
     }
 
+    @Deprecated
     public static <T> ImmutableList<T> getInstancesOfTypeOrFail(Stream<?> stream, Class<T> clazz) {
         return streamInstancesOfTypeOrFail(stream, clazz).collect(ImmutableList.toImmutableList());
     }
--- OcavaCore/src/test/java/com/ocadotechnology/utils/TypesTest.java
+++ OcavaCore/src/test/java/com/ocadotechnology/utils/TypesTest.java
@@ -15,6 +15,9 @@
  */
 package com.ocadotechnology.utils;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -45,5 +48,125 @@ public class TypesTest {
     @Test
     public void testGetInstancesOfTypeUntil_negativeNumberOccurs() throws Exception {
         Assertions.assertEquals(ImmutableList.of(1.0, 3.0), Types.getInstancesOfTypeUntil(Lists.newArrayList(1.0, 2, 3.0, -4.0, 5.0), Double.class, number -> number.intValue() >=  0));
+    }
+
+    @Test
+    public void testSafeStreamInstancesOfType_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.safeStreamInstancesOfType(Lists.newArrayList(), Object.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(), Types.safeStreamInstancesOfType(Stream.of(), Object.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testSafeStreamInstancesOfType_whenGivenSomeValuesOfType_resultsCorrectlyFiltered() {
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.safeStreamInstancesOfType(List.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.safeStreamInstancesOfType(Stream.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testSafeGetInstancesOfType_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.safeGetInstancesOfType(Lists.newArrayList(), Object.class));
+        Assertions.assertEquals(ImmutableList.of(), Types.safeGetInstancesOfType(Stream.of(), Object.class));
+    }
+
+    @Test
+    public void testSafeGetInstancesOfType_whenGivenSomeValuesOfType_resultsCorrectlyFiltered() {
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.safeGetInstancesOfType(List.of(1, 2.1, 3), Integer.class));
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.safeGetInstancesOfType(Stream.of(1, 2.1, 3), Integer.class));
+    }
+
+    @Test
+    public void testSafeStreamInstancesOfTypeOrFail_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.safeStreamInstancesOfTypeOrFail(Lists.newArrayList(), Object.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(), Types.safeStreamInstancesOfTypeOrFail(Stream.of(), Object.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testSafeStreamInstancesOfTypeOrFail_whenGivenSomeValuesOfType_thenThrows() {
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.safeStreamInstancesOfTypeOrFail(List.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.safeStreamInstancesOfTypeOrFail(Stream.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testSafeStreamInstancesOfTypeOrFail_whenGivenAllValuesOfType_thenResultsCorrectlyReturned() {
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.safeStreamInstancesOfTypeOrFail(List.of(1, 2, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.safeStreamInstancesOfTypeOrFail(Stream.of(1, 2, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testSafeGetInstancesOfTypeOrFail_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.safeGetInstancesOfTypeOrFail(Lists.newArrayList(), Object.class));
+        Assertions.assertEquals(ImmutableList.of(), Types.safeGetInstancesOfTypeOrFail(Stream.of(), Object.class));
+    }
+
+    @Test
+    public void testSafeGetInstancesOfTypeOrFail_whenGivenSomeValuesOfType_thenThrows() {
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.safeGetInstancesOfTypeOrFail(List.of(1, 2.1, 3), Integer.class));
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.safeGetInstancesOfTypeOrFail(Stream.of(1, 2.1, 3), Integer.class));
+    }
+
+    @Test
+    public void testSafeGetInstancesOfTypeOrFail_whenGivenAllValuesOfType_thenResultsCorrectlyReturned() {
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.safeGetInstancesOfTypeOrFail(List.of(1, 2, 3), Integer.class));
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.safeGetInstancesOfTypeOrFail(Stream.of(1, 2, 3), Integer.class));
+    }
+
+    @Test
+    public void testUnunsafeStreamInstancesOfType_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeStreamInstancesOfType(Lists.newArrayList(), Object.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeStreamInstancesOfType(Stream.of(), Object.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testUnunsafeStreamInstancesOfType_whenGivenSomeValuesOfType_resultsCorrectlyFiltered() {
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.unsafeStreamInstancesOfType(List.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.unsafeStreamInstancesOfType(Stream.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testUnunsafeGetInstancesOfType_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeGetInstancesOfType(Lists.newArrayList(), Object.class));
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeGetInstancesOfType(Stream.of(), Object.class));
+    }
+
+    @Test
+    public void testUnunsafeGetInstancesOfType_whenGivenSomeValuesOfType_resultsCorrectlyFiltered() {
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.unsafeGetInstancesOfType(List.of(1, 2.1, 3), Integer.class));
+        Assertions.assertEquals(ImmutableList.of(1, 3), Types.unsafeGetInstancesOfType(Stream.of(1, 2.1, 3), Integer.class));
+    }
+
+    @Test
+    public void testUnunsafeStreamInstancesOfTypeOrFail_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeStreamInstancesOfTypeOrFail(Lists.newArrayList(), Object.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeStreamInstancesOfTypeOrFail(Stream.of(), Object.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testUnunsafeStreamInstancesOfTypeOrFail_whenGivenSomeValuesOfType_thenThrows() {
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.unsafeStreamInstancesOfTypeOrFail(List.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.unsafeStreamInstancesOfTypeOrFail(Stream.of(1, 2.1, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testUnunsafeStreamInstancesOfTypeOrFail_whenGivenAllValuesOfType_thenResultsCorrectlyReturned() {
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.unsafeStreamInstancesOfTypeOrFail(List.of(1, 2, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.unsafeStreamInstancesOfTypeOrFail(Stream.of(1, 2, 3), Integer.class).collect(ImmutableList.toImmutableList()));
+    }
+
+    @Test
+    public void testUnunsafeGetInstancesOfTypeOrFail_whenGivenAnEmptyCollection_noResultsAreReturned() {
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeGetInstancesOfTypeOrFail(Lists.newArrayList(), Object.class));
+        Assertions.assertEquals(ImmutableList.of(), Types.unsafeGetInstancesOfTypeOrFail(Stream.of(), Object.class));
+    }
+
+    @Test
+    public void testUnunsafeGetInstancesOfTypeOrFail_whenGivenSomeValuesOfType_thenThrows() {
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.unsafeGetInstancesOfTypeOrFail(List.of(1, 2.1, 3), Integer.class));
+        Assertions.assertThrows(IllegalStateException.class, () -> Types.unsafeGetInstancesOfTypeOrFail(Stream.of(1, 2.1, 3), Integer.class));
+    }
+
+    @Test
+    public void testUnunsafeGetInstancesOfTypeOrFail_whenGivenAllValuesOfType_thenResultsCorrectlyReturned() {
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.unsafeGetInstancesOfTypeOrFail(List.of(1, 2, 3), Integer.class));
+        Assertions.assertEquals(ImmutableList.of(1, 2, 3), Types.unsafeGetInstancesOfTypeOrFail(Stream.of(1, 2, 3), Integer.class));
     }
 }
